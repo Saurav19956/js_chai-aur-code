7affde7615e9648bbf8d38fde66f42f124331661
Saurav19956 <135837844+Saurav19956@users.noreply.github.com>
2024-08-27 07:37:19 +0000

    variables

---
+++ 01-begin/variable.js
@@ -0,0 +1,27 @@
+
+const accountId = 12324;
+
+let email = "saurav@gmail.com";
+
+var password = "123456";
+
+accountCity = 'Jaipur';  // without declaring key (const,var, let) reverse memory space ;
+
+let accountState;  // only declare but not assign value will be undefined
+
+
+// accountId= 12  Not allowed
+
+email = "sam@gmail.com" // allowed
+
+password = "123344"
+
+accountCity = "bangluru"
+
+console.table([accountId, email, password, accountCity, accountState]);
+
+
+// only use let for functional and block scope , block scope is like {}, avoid var
+
+// var create problem if same variable is define inside the function or block scope value will be changed through out file with same variable
+
