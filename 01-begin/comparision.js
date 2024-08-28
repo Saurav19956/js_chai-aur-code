ed76d6e5c405929d1bba80747bbc1bd1a6c0ceb6
Saurav19956 <135837844+Saurav19956@users.noreply.github.com>
2024-08-28 06:56:52 +0000

    string prototypes

---
+++ 01-begin/comparision.js
@@ -0,0 +1,40 @@
+/*
+Note : - Always comparision operand should be same dataType
+==  euqlity check
+>=, <=
+!
+return boolean
+*/
+
+
+// console.log('2'>1) --. true
+// console.log('08'> 7); --> true
+
+
+let compariosnValue = undefined==0 
+
+console.log(compariosnValue);
+
+/*
+null > 0 --> false;
+null>=0  --> true;
+null==0 --> false
+
+undefined > 0 --> false
+undefined >= 0 --> false
+undefined == 0 --> false
+*/
+
+/*
+primitive 7 category--> string, null, undefined, number(decimal, int), bigInt, symbol, boolean
+call by value
+
+Non-primitive --> array, object, function. --> typeof object even in case of funtion , typeof function but actul function object
+call by referance
+*/
+
+// const call = ['sam',1,2,43];
+// console.log(typeof call);
+
+
+
